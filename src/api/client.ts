ce4130e6d00583300ef57a83d5ea2beb3b4636f2
Micheal <michealbehailu94@gmail.com>
2026-05-01 12:06:02 +0300

    Implementing Api client

---
+++ src/api/client.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:8082/api';
+
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export default apiClient;
